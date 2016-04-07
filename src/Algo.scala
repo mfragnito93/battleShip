8915b9173bb86621232d8e3622e0fefc242a4d77
mfragnito93 <mfragnito93@gmail.com>
2016-04-07 18:13:39 -0400

    Added algo transpose, matrixSum, constructMap

--- src/Algo.scala
+++ src/Algo.scala
@@ -6,7 +6,7 @@ import scala.collection.mutable.ArrayBuffer
 class Algo {
 
   //Split the game map into arrays for effective heat map calculations
-  def split(arrayList: ArrayBuffer[ArrayBuffer[Int]], subList: Int, value: Int): ArrayBuffer[Object] = {
+  def split(arrayList: ArrayBuffer[ArrayBuffer[Int]], subList: Int, value: Int): ArrayBuffer[ArrayBuffer[Int]] = {
 
     /*1)Take a row in which the value you wish to split on ie the attack coordinates
       3)Using the sublist in which value sits we can split the row further
@@ -15,7 +15,7 @@ class Algo {
 
     var tmp = ArrayBuffer[Int]() //first half split [0,1,2,3]
     var tmp2 = ArrayBuffer[Int]() //second half split [5,6,7,8,]9
-    var out = ArrayBuffer[Object]() //final output
+    var out = ArrayBuffer[ArrayBuffer[Int]]() //final output
 
     val goTo = arrayList(subList).indexOf(value) //find where in the listthe value is that you want to split on
     for (j <- arrayList.indices) {
@@ -38,5 +38,35 @@ class Algo {
       }
     }
     out
+  }
+
+  def transpose(matrix: Array[Array[Int]]): Array[Array[Int]]={
+    val cols=matrix(0).length
+    val rows=matrix.length
+    val matrixT=Array.ofDim[Int](cols,rows)
+
+    for(i<-0 until cols; j<-0 until rows){
+        matrixT(i)(j)=matrix(j)(i)
+    }
+    matrixT
+  }
+
+  def sumMatrix(m1: Array[Array[Int]],m2: Array[Array[Int]]): Array[Array[Int]]={
+    for (i<-m1.indices;j<-m1(0).indices){
+      m1(i)(j)+=m2(i)(j)
+    }
+    m1
+  }
+
+  def constructMap(rows: Int,cols: Int): Map[Int,ArrayBuffer[ArrayBuffer[Int]]]={
+    var map = Map[Int,ArrayBuffer[ArrayBuffer[Int]]]()
+    var tmp = ArrayBuffer[Int]()
+    for(i<-0 until cols) {
+      tmp += i
+    }
+    for(j<-0 until rows){
+      map=map+(j->ArrayBuffer(tmp))
+    }
+    map
   }
 }
--- src/algoTest.scala
+++ src/algoTest.scala
@@ -7,7 +7,11 @@ object algoTest {
   def main(args:Array[String]): Unit ={
     var algo=new Algo
     val m1=ArrayBuffer(ArrayBuffer(0,1,2,3,4,5,6,7,8,9))
-    println(algo.split(m1,0,5))
+    val m1a=Array(Array(0,1,2,3,4,5,6,7,8,9),Array(0,1,2,3,4,5,6,7,8,9))
+    var map=algo.constructMap(5,5)
+    println(map)
+    map=map+(0->algo.split(map(0),0,2))
+    println(map)
   }
 
 }
